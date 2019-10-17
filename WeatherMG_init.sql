96bdecb26dcbbcfbdc3a5917ac0a53b2768465a1
tutamasha <tutamasha@gmail.com>
2019-10-17 17:56:23 -0400

    Adding DimDate dimension

--- WeatherMG_init.sql
+++ WeatherMG_init.sql
@@ -154,3 +154,70 @@ CREATE TABLE [dbo].[PackageExecutionStamp](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+
+DECLARE @StartDate DATE = '18400101', @NumberOfYears INT = 130;
+
+-- prevent set or regional settings from interfering with 
+-- interpretation of dates / literals
+
+SET DATEFIRST 7;
+SET DATEFORMAT mdy;
+SET LANGUAGE US_ENGLISH;
+
+DECLARE @CutoffDate DATE = DATEADD(YEAR, @NumberOfYears, @StartDate);
+
+-- this is just a holding table for intermediate calculations:
+If not exists (select * from sys.tables where name = 'dimdate')
+begin
+CREATE TABLE DimDate
+(
+  [Date]       DATE PRIMARY KEY, 
+  [Day]        AS DATEPART(DAY,      [date]),
+  [Month]      AS DATEPART(MONTH,    [date]),
+  [FirstOfMonth] AS CONVERT(DATE, DATEADD(MONTH, DATEDIFF(MONTH, 0, [date]), 0)),
+  [MonthName]  AS DATENAME(MONTH,    [date]),
+  [Week]       AS DATEPART(WEEK,     [date]),
+  [ISOweek]    AS DATEPART(ISO_WEEK, [date]),
+  [DayOfWeek]  AS DATEPART(WEEKDAY,  [date]),
+  [DayName]	   AS DATENAME(dw, [date]),
+  [quarter]    AS DATEPART(QUARTER,  [date]),
+  [year]       AS DATEPART(YEAR,     [date]),
+  FirstOfYear  AS CONVERT(DATE, DATEADD(YEAR,  DATEDIFF(YEAR,  0, [date]), 0)),
+  Style112     AS CONVERT(CHAR(8),   [date], 112),
+  Style101     AS CONVERT(CHAR(10),  [date], 101)
+);
+
+-- use the catalog views to generate as many rows as we need
+
+INSERT DimDate([date]) 
+SELECT d
+FROM
+(
+  SELECT d = DATEADD(DAY, rn - 1, @StartDate)
+  FROM 
+  (
+    SELECT TOP (DATEDIFF(DAY, @StartDate, @CutoffDate)) 
+      rn = ROW_NUMBER() OVER (ORDER BY s1.[object_id])
+    FROM sys.all_objects AS s1
+    CROSS JOIN sys.all_objects AS s2
+    -- on my system this would support > 5 million days
+    ORDER BY s1.[object_id]
+  ) AS x
+) AS y;
+
+
+ALTER TABLE DimDate 
+ADD Primary Key (DateKey)
+
+ALTER TABLE [dbo].[FactWeatherDaily] ADD FOREIGN KEY (Date) 
+REFERENCES DimDate(Date);
+
+ALTER TABLE[dbo].[FactWeatherMonthly]
+ADD FOREIGN KEY (Date) 
+REFERENCES DimDate(Date);
+
+ALTER TABLE [dbo].[FactWeather3h] ADD FOREIGN KEY (Date) 
+REFERENCES DimDate(Date);
+
+end
